6ad9ec136acaf4e38a53cd0ed31159f5494ea1be
barlotem <barlotem@gmail.com>
2022-07-03 09:22:04 +0300

    Update: change Task to Stateless component

--- react/todo-list/src/components/task/Task.jsx
+++ react/todo-list/src/components/task/Task.jsx
@@ -1,36 +1,32 @@
-import { Component } from 'react';
+import React from 'react';
 
-class Task extends Component {
-	state = {};
-
-	render() {
-		return (
-			<tr>
-				<td>
-					<input
-						name='isComplete'
-						type='checkbox'
-						checked={this.props.task.isComplete}
-						onChange={(event) => {
-							const target = event.target;
-							const value =
-								target.type === 'checkbox' ? target.checked : target.value;
-							this.props.onUpdateCompleteStatus(value, this.props.task.id);
-						}}
-					/>
-				</td>
-				<td>{this.props.task.description}</td>
-				<td>
-					<button
-						className='btn btn-danger btn-sm'
-						onClick={() => this.props.onDelete(this.props.task.id)}
-					>
-						Delete
-					</button>
-				</td>
-			</tr>
-		);
-	}
-}
+const Task = (props) => {
+	return (
+		<tr>
+			<td>
+				<input
+					name='isComplete'
+					type='checkbox'
+					checked={props.task.isComplete}
+					onChange={(event) => {
+						const target = event.target;
+						const value =
+							target.type === 'checkbox' ? target.checked : target.value;
+						props.onUpdateCompleteStatus(value, props.task.id);
+					}}
+				/>
+			</td>
+			<td>{props.task.description}</td>
+			<td>
+				<button
+					className='btn btn-danger btn-sm'
+					onClick={() => props.onDelete(props.task.id)}
+				>
+					Delete
+				</button>
+			</td>
+		</tr>
+	);
+};
 
 export default Task;
